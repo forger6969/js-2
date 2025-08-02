8801fa1257c5692e1777303164c72453ddd69aa1
forger6969 <saidazim186@gmail.com>
2025-08-02 21:29:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let name = prompt('Ваше имя?')
+
+let age = prompt('Ваш возраст?')
+
+let adress = prompt ('Где вы живете?')
+
+
+let nameotvet =  ' Привет! Меня зовут ' 
+
+let  ageotvet =  ' мне ' 
+
+let adressotvet = ' я живу в '
+
+
+let rezultat = alert( nameotvet  + name + ageotvet + age + adressotvet + adress )
+
